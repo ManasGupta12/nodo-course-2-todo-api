32163ea10f4f304eecec8f4fa95a209d2e1ad6ae
Manas <iammanas95@gmail.com>
2018-06-16 14:05:00 +0530

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express=require('express');
 
 var body=require('body-parser');
 
+const {ObjectID}=require('mongodb');
 
 var {mongoose}=require('./mongoose.js')
 
@@ -31,6 +32,24 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(e);
    });
 });
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id))
+  {
+return res.status(404).send();
+}
+todo.findById(id).then((todo)=>{
+	if(!todo){
+		return res.status(404).send();
+	}
+	res.send({todo});
+}).catch((e)=>{
+	res.status(400).send()
+});
+});
+
+   
 
 
 app.listen(3000,()=>{
